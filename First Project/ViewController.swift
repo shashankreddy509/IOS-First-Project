8c97b0fc1a9a6bc5e947bf238e252a19acc558e0
Shashank Reddy <Shashank.r@rentomojo.com>
2016-06-27 10:29:56 +0530

    Initial Commit

---
+++ First Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by Shashank Reddy on 27/06/16.
+//  Copyright © 2016 Shashank Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
